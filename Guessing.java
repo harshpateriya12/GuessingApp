f95ecde6c07ec0e04ec028b3d04eae17d3362f30
harshpateriya12 <harsh.pateriya1@gmail.com>
2026-02-23 16:28:36 -0800

    Coded UC4 for Invalid Input

--- Guessing.java
+++ Guessing.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 public class Guessing {
-	public static void main(String[] args){
+	public static void main(String[] args) throws InvalidInputException{
 		System.out.println("Welcome to guessing app");
 		GameConfig game = new GameConfig();
 		game.showRules();
@@ -11,7 +11,20 @@ public class Guessing {
 		
 		while(attempts < game.getMaxAttempts()){
 			System.out.print("Enter your guess");
-			int guess = sc.nextInt();
+			//int guess = ValidationService.validateInput(sc.nextLine());
+			
+			String input = sc.nextLine();
+
+            int guess;
+            try {
+                guess = ValidationService.validateInput(input);
+            } catch (InvalidInputException e) {
+                // Do not count invalid input as an attempt; just inform and continue
+                System.out.println(e.getMessage());
+                System.out.println(); // blank line for readability
+                continue;
+            }
+
 			attempts++;
 			
 			
